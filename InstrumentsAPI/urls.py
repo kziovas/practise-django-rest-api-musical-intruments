bf4fdc0aa035a58021cf2b526c323457eaf07f6b
Kostas <Ziovas>
2021-11-27 19:44:27 +0200

    Fixed injector, completed docker compose and made conainirization functional

--- InstrumentsAPI/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('Instruments.routing.instruments_urls', namespace='instrument_api'))
-]
