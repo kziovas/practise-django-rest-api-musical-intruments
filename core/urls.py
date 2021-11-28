4e2c3798e4e925f14cc96c81c77c12ebc2ce48c4
Kostas <Ziovas>
2021-11-28 13:27:00 +0200

    Improved api url ad removed unused file with docker commands

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('Instruments.routing.instruments_urls', namespace='instrument_api'))
+    path("admin/", admin.site.urls),
+    path(
+        "api/instruments",
+        include("Instruments.routing.instruments_urls", namespace="instrument_api"),
+    ),
 ]
